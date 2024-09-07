93dc08ad99a313230add0ba66a6ab74092fb5a8a
Trong <spektralfrogadier@gmail.com>
2024-09-06 20:55:43 -0700

    pass nixpkgs argument

--- modules/sleepy.nix
+++ modules/sleepy.nix
@@ -2,27 +2,27 @@
 { config, pkgs, lib, ... }:
 
 let
-  dmenu = pkgs.callPackage ../default.nix { app = "dmenu"; };
-  st = pkgs.callPackage ../default.nix { app = "st"; };
-  slstatus = pkgs.callPackage ../default.nix { app = "slstatus"; };
+  dmenu = pkgs.callPackage ../default.nix { app = "dmenu"; nixpkgs = pkgs; };
+  st = pkgs.callPackage ../default.nix { app = "st"; nixpkgs = pkgs; };
+  slstatus = pkgs.callPackage ../default.nix { app = "slstatus"; nixpkgs = pkgs; };
 in
 {
   options.sleepy.enableDmenu = lib.mkOption {
     type = lib.types.bool;
     default = false;
-    description = "Enable the Sleepy Dmenu build";
+    description = "Enable the Sleepy Dmenu build.";
   };
 
   options.sleepy.enableSlstatus = lib.mkOption {
     type = lib.types.bool;
     default = false;
-    description = "Enable the Sleepy Slstatus build";
+    description = "Enable the Sleepy Slstatus build.";
   };
 
   options.sleepy.enableST = lib.mkOption {
     type = lib.types.bool;
     default = false;
-    description = "Enable the Sleepy ST build";
+    description = "Enable the Sleepy ST build.";
   };
 
   config = {
@@ -35,4 +35,3 @@ in
     ];
   };
 }
-
